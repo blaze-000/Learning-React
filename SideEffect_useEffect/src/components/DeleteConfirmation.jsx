7ba4ae0e8fc8bdec1522bc57133dda8331afa5f5
Asmit Kumar Sah <asmitsah36@gmail.com>
2025-09-13 18:45:36 +0545

    Learned new Hook - UseEffect

---
+++ SideEffect_useEffect/src/components/DeleteConfirmation.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
+
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, SetRemainingTime] = useState(TIMER);
+
+ useEffect(() => {
+   const inter =setInterval(() => {
+    SetRemainingTime((prev) => prev - 10);
+  }, 10);
+ 
+ return () => {
+  clearInterval(inter);
+  }
+ }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
+  return (
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
+      </div>
+      <progress value={remainingTime} max={TIMER} />
+    </div>
+  );
+}
