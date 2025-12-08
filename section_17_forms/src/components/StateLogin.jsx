2228e0efd0a1e501ec53f93ebdfd2a81b7333412
Asmit Kumar Sah <asmitsah36@gmail.com>
2025-12-08 09:56:13 +0545

    feat: add Signup and StateLogin components with form handling

---
+++ section_17_forms/src/components/StateLogin.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Login() {
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleInputChange(identifier, e){
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: e.target.value
+    }))
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+     console.log(enteredValues);
+  }
+ 
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" value={enteredValues.email} onChange={(e) => handleInputChange('email', e)} />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" value={enteredValues.password} onChange={(e) => handleInputChange('password', e)} />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
